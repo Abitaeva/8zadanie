ce00b0ad14df9b45d3aaf876fd7fde62b5dd8b12
Абитаева <anazyken@gmail.com>
2024-02-04 22:52:15 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/zadanie8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zadanie8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Container(
+      color: Colors.white,
+      child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Americas_%28orthographic_projection%29.svg/1200px-Americas_%28orthographic_projection%29.svg.png"),
+    ),
+  );
+}
